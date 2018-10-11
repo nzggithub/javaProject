ad43831a4fd7d93a3b2f906624b219e09c86378a
ningzg <329800183@qq.com>
2018-10-11 17:10:50 +0800

    的说法

--- parent/nzg-jvm/src/main/java/com/ningzhengao/jvm/classLoader/MyClassLoader.java
+++ parent/nzg-jvm/src/main/java/com/ningzhengao/jvm/classLoader/MyClassLoader.java
@@ -2,43 +2,52 @@ package com.ningzhengao.jvm.classLoader;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * 自定义classloader,实现热部署
+ */
 public class MyClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) {
-        String myPath = "D:\\mycode\\javaProject\\parent\\nzg-jvm\\" + name.replace(".", "/") + ".class";
+        String myPath = Thread.currentThread().getContextClassLoader().getResource("./") + name.replace(".", "/") + ".class";
         System.out.println(myPath);
         byte[] cLassBytes = null;
         Path path = null;
         try {
-            path = Paths.get(myPath);
+            path = Paths.get(new URI(myPath));
             cLassBytes = Files.readAllBytes(path);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        Class clazz = defineClass("com.ningzhengao.jvm.classLoader." + name, cLassBytes, 0, cLassBytes.length);
+        Class clazz = defineClass(name, cLassBytes, 0, cLassBytes.length);
         return clazz;
     }
 
     public static void main(String[] args) {
         try {
-            System.out.println(Thread.currentThread().getContextClassLoader().getResource("./com/ningzhengao/jvm/classLoader/MyTestClass.class"));
-            MyClassLoader classLoader = new MyClassLoader();
-            Class<?>c = classLoader.findClass("MyTestClass");
-            //获取实例
-            Object instance = c.newInstance();
-            //获取方法
-            Method method = c.getDeclaredMethod("show");
-            //执行实例的方法
-            method.invoke(instance);
-
+            //初次编译
             new MyTestClass().show();
+            System.out.println("-----------");
+            //实时编译
+            while (true) {
+                MyClassLoader classLoader = new MyClassLoader();
+                Class<?> c = classLoader.findClass("com.ningzhengao.jvm.classLoader.MyTestClass");
+                //获取实例
+                Object instance = c.newInstance();
+                //获取方法
+                Method method = c.getDeclaredMethod("show");
+                //执行实例的方法
+                method.invoke(instance);
+                Thread.sleep(3000L);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
 
